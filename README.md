e4a547bc656ef94598d6e15c3c62a57c39df4710
Gabriel <gabrielkellersouza@gmail.com>
2023-07-25 17:59:10 -0300

    Update code and readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # imagem_overlay_with_python
+
+# Programming Language used:
+
+    Python 3.11.2
+    To start the program just run the file make_water_and_instagram_story.py
+    
+# About the program:
+    
+    This is a program aimed at those who wish to automatically add a watermark to an image. Here you will find the necessary logic for that.
+    You will also find a way to identify if an image is in a 9:16 format.
+    This is not a program developed with common users in mind; reading and coding skills are necessary for its implementation.
+
+# Solutions to possible problems
+
+    If you encounter any issues with JPEG images, modify the code in the 'overlay_image_default' function. This is a problem that usually occurs when working with JPEG images.
+
+    new_image = Image.new("RGB", background.size, (0, 0, 0))
+
+# More about the development
+
+    To avoid problems with images of different sizes, a resizing to 1080px was made, respecting the aspect ratio of the respective image.
+    Creating a watermark with a width of 1080px made it much easier to place it on the image.
+    There are two backgrounds available for images in the 9:16 format.
--- make_water_and_instagram_story.py
+++ make_water_and_instagram_story.py
@@ -1,66 +1,35 @@
 from PIL import Image
 
-# def overlay_image_default(background_path, overlay_path, output_path):
-#     background = Image.open(background_path)
-#     overlay = Image.open(overlay_path)
-
-#     # No meio em baixo 20px de distância
-#     x = (background.width - overlay.width) // 2
-#     y = background.height - overlay.height - 20
-
-#     # Crie uma nova imagem com o mesmo tamanho da imagem de fundo
-#     new_image = Image.new("RGBA", background.size, (0, 0, 0, 0))
-
-#     # Cole a imagem de fundo e a imagem de sobreposição na nova imagem
-#     new_image.paste(background, (0, 0))
-#     new_image.paste(overlay, (x, y), overlay)
-
-#     # Salve a imagem resultante em PNG para preservar a transparência
-#     new_image.save(output_path)
 
 def resize_background(background_path, new_width):
-    # Abre a imagem de fundo usando a biblioteca Pillow
     background = Image.open(background_path)
-
-    # Calcula a proporção para manter a largura da imagem de fundo
     width_percent = (new_width / float(background.size[0]))
     new_height = int((float(background.size[1]) * float(width_percent)))
-
-    # Redimensiona a imagem de fundo mantendo a proporção
     resized_background = background.resize((new_width, new_height), Image.LANCZOS)
 
     return resized_background
 
 
 def overlay_image_default(background_path, overlay_path, output_path):
-    # Redimensiona a imagem de fundo para 1080 pixels de largura
     background = resize_background(background_path, 1080)
 
     overlay = Image.open(overlay_path)
 
-    # No meio em baixo 20px de distância
     x = (background.width - overlay.width) // 2
     y = 0
 
-    # Crie uma nova imagem com o mesmo tamanho da imagem de fundo
     new_image = Image.new("RGBA", background.size, (0, 0, 0, 0))
 
-    # Cole a imagem de fundo e a imagem de sobreposição na nova imagem
     new_image.paste(background, (0, 0))
     new_image.paste(overlay, (x, y), overlay)
-
-    # Salve a imagem resultante em PNG para preservar a transparência
     new_image.save(output_path)
 
 
 def resize_and_overlay_story(
         background_path, overlay_path, output_path):
-    # Abre as imagens usando a biblioteca Pillow
+    
     background = Image.open(background_path)
     overlay = Image.open(overlay_path)
-
-    # Redimensiona a imagem "image_normal.png" para
-    # ter a largura de 1620 pixels
     target_width = 1620
     width_percent = (target_width / float(overlay.width))
     target_height = int((float(overlay.height) * float(width_percent)))
@@ -75,14 +44,12 @@ def resize_and_overlay_story(
     x = (background.width - overlay.width) // 2
     y = (background.height - overlay.height) // 2
 
-    # Crie uma nova imagem com o mesmo tamanho do background
     new_image = Image.new("RGB", background.size)
 
     new_image.paste(background, (0, 0))
     new_image.paste(overlay, (x, y))
 
-    # Salve a imagem resultante em JPEG
-    new_image.save(output_path, format='PNG')
+    new_image.save(output_path, format='JPEG')
 
 
 def resize_and_overlay_story_vertical(
@@ -105,23 +72,22 @@ def resize_and_overlay_story_vertical(
     x = (background.width - overlay.width) // 2
     y = (background.height - overlay.height) // 2
 
-    # Crie uma nova imagem com o mesmo tamanho do background
     new_image = Image.new("RGB", background.size)
 
     new_image.paste(background, (0, 0))
     new_image.paste(overlay, (x, y))
 
-    # Salve a imagem resultante em JPEG
-    new_image.save(output_path, format='PNG')
+    new_image.save(output_path, format='JPEG')
 
 
 def is_9_16_aspect_ratio(image_path):
     image = Image.open(image_path)
 
-    desired_aspect_ratio = 9 / 16
+    desired_aspect_ratio = 0.5625
+    print(desired_aspect_ratio)
     aspect_ratio = image.width / image.height
 
-    error_margin = 0.01
+    error_margin = 0.10
 
     return abs(aspect_ratio - desired_aspect_ratio) < error_margin
 
@@ -130,7 +96,6 @@ def main(image):
 
     background_image_path = image
     overlay_image_path = "icon.png"
-    # overlay_image_path = "g57320.png"
     output_image_path = "imagem_1080.png"
 
     overlay_image_default(
